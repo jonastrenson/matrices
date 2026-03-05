43a3f04ef32f5897b7fee46f3a68cfc37d8b07ed
Trens <Trens@10.45.243.166>
2026-03-05 10:57:35 +0100

    Testcase

---
+++ matrices/src/matrices/Immutable_MatrixTest.java
@@ -0,0 +1,14 @@
+package matrices;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Immutable_MatrixTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
