4c57221c188e37e155a630f0b52d3664ddf6a5b5
Trens <Trens@10.45.243.166>
2026-03-05 14:12:51 +0100

    test op representation exposure toegevoegd

--- matrices/src/matrices/Immutable_MatrixTest.java
+++ matrices/src/matrices/Immutable_MatrixTest.java
@@ -33,7 +33,31 @@ class Immutable_MatrixTest {
 		assertEquals(M3.getElement(1,2) ,6);
 		assertEquals(M3.getElement(2,1) ,9);
 		assertEquals(M3.getElement(2,2) ,12);
+		
+		// test op representationexposure 
+		// test 1
+		double[] myArray = new double[] {1,2,3,4};
+		Immutable_Matrix M4 = new Immutable_Matrix(2,2,myArray);
+		myArray = new double[] {5,6,7,8};
+		assertEquals(M4.numberOfRows(), 2);
+		assertEquals(M4.numberOfColumns(), 2);
+		assertEquals(M4.getElement(1,1) ,1);
+		assertEquals(M4.getElement(1,2) ,2);
+		assertEquals(M4.getElement(2,1) ,3);
+		assertEquals(M4.getElement(2,2) ,4);
 
+		assertArrayEquals(M4.toRowMajorArray(), new double[] {1,2,3,4});
+		assertArrayEquals(M4.toColumnMajorArray(), new double[] {1,3,2,4});
+		assertArrayEquals(M4.toArrayOfRows(), new double[][] {{1,2},{3,4}});
+		
+		//test 2
+		Immutable_Matrix M5 = Immutable_Matrix.plus(M1, M2);
+		M1 = M4;
+		assertEquals(M5.getElement(1,1) ,3);
+		assertEquals(M5.getElement(1,2) ,6);
+		assertEquals(M5.getElement(2,1) ,9);
+		assertEquals(M5.getElement(2,2) ,12);
+		
 
 		
 	}
